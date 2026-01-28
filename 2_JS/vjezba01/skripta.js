d57662c66b5817a438bff630879591ac6298ed81
lhlavati <lukahlavati@gmail.com>
2026-01-28 11:35:45 +0100

    Upoznavanje sa JS

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,11 @@
+console.info('Hello world 5');
+// console.table(JSON.parse('{"name":"John", "age":30, "car":null}'));
+
+console.log('O\'Neal');
+console.log("O'Neal");
+
+console.log(`Hello
+    World
+    6`);
+
+document.getElementById('ispis').innerHTML='<a href="#">Poveznica</a>';
