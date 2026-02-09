c3c4dfc96db2daba150c0891b0092e47c6ce3b8f
lhlavati <lukahlavati@gmail.com>
2026-02-09 12:33:45 +0100

    napravljen kostur js-a za rješavanje zad

--- 2_JS/vjezba06/zad/app.js
+++ 2_JS/vjezba06/zad/app.js
@@ -1,1 +1,40 @@
+const rezultat = document.getElementById("rezultat");
+document.getElementById("izvedi").addEventListener("click", () => {
+    rezultat.innerHTML = "";
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const zadatak = document.getElementById("zadatak").value;
 
+    switch(zadatak) {
+        case "1":
+            
+            return;
+        case "2":
+            
+            return;
+        case "3":
+            
+            return;
+        case "4":
+            
+            return;
+        case "5":
+            
+            return;
+        case "6":
+            
+            return;
+        case "7":
+            
+            return;
+        case "8":
+            
+            return;
+        case "9":
+            
+            return;
+        case "10":
+            
+            return;
+    }
+});
