8dfe920963fd705db8f3a969db51fe3b97508bdf
lhlavati <lukahlavati@gmail.com>
2026-02-10 10:14:48 +0100

    gotovi zadaci

--- 2_JS/vjezba06/zad/app.js
+++ 2_JS/vjezba06/zad/app.js
@@ -6,8 +6,8 @@ document.getElementById("izvedi").addEventListener("click", () => {
   const zadatak = document.getElementById("zadatak").value;
   var x, y;
   var arr = [];
+  var temp = [];
   var index;
-  var temp;
 
   switch (zadatak) {
     case "1":
@@ -86,46 +86,89 @@ document.getElementById("izvedi").addEventListener("click", () => {
       arr = [a, 123, "zadatak5", true, null, undefined, Symbol];
       x = Number(a);
 
-      if(a === ""){
+      if (a === "") {
         arr[0] = undefined;
-      } else if(!isNaN(x)){
+      } else if (!isNaN(x)) {
         arr[0] = Number(x);
-      } else if(a.toLowerCase() === "true"){
+      } else if (a.toLowerCase() === "true") {
         arr[0] = true;
-      } else if(a.toLowerCase() === "false"){
+      } else if (a.toLowerCase() === "false") {
         arr[0] = false;
       } else {
         arr[0] = a;
       }
 
       switch (typeof arr[0]) {
-            case "string":
-                rezultat.innerHTML = `${arr[0]} je string.`;
-                break;
-            case "number":
-                rezultat.innerHTML = `${arr[0]} je broj.`;
-                break;
-            case "boolean":
-                rezultat.innerHTML = `${arr[0]} je boolean.`;
-                break;
-            case "undefined":
-                rezultat.innerHTML = `a je undefined.`;
-                break;
-            default:
-                rezultat.innerHTML = "Greška";
-                break;
-        }
+        case "string":
+          rezultat.innerHTML = `${arr[0]} je string.`;
+          break;
+        case "number":
+          rezultat.innerHTML = `${arr[0]} je broj.`;
+          break;
+        case "boolean":
+          rezultat.innerHTML = `${arr[0]} je boolean.`;
+          break;
+        case "undefined":
+          rezultat.innerHTML = `a je undefined.`;
+          break;
+        default:
+          rezultat.innerHTML = "Greška";
+          break;
+      }
 
       return;
     case "6":
+      if (!a || !b) {
+        rezultat.innerHTML = "Polja ne mogu biti prazna!";
+        return;
+      }
+      arr = [a, b, "KRAJ"];
+      arr.length = arr.length - 1;
+      rezultat.innerHTML = arr;
       return;
     case "7":
+      if (!a || !b) {
+        rezultat.innerHTML = "Polja ne mogu biti prazna!";
+        return;
+      }
+      arr = [a, 2];
+      temp = [b, 4];
+      rezultat.innerHTML = [arr, ...temp];
       return;
     case "8":
+      if (!a || !b) {
+        rezultat.innerHTML = "Polja ne mogu biti prazna!";
+        return;
+      }
+      arr = [a, ,b];
+      rezultat.innerHTML = arr[1];
       return;
     case "9":
+      if (!a || !b) {
+        rezultat.innerHTML = "Polja ne mogu biti prazna!";
+        return;
+      }
+      arr = [a, b, 7, 6];
+      var [first, ...rest] = arr;
+      rezultat.innerHTML = "Prvi: " + first + "<br>Ostatak: " + rest;
       return;
     case "10":
+      x = Number(b);
+      switch (true) {
+        case a === "" || b === "":
+            arr = [];
+            break;
+        case a === b:
+            arr = [a];
+            break;
+        case !isNaN(x) && x === 2:
+            arr = [a, b];
+            break;
+        default:
+            arr = [a, b, "x"];
+            break;
+      }
+      rezultat.innerHTML = "Array: " + arr;
       return;
   }
 });
