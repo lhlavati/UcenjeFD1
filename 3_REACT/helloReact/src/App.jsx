e46fa51e7cbb05b1bf0428028d17f07841f0fdaa
Luka Hlavati <lukahlavati@gmail.com>
2026-03-10 18:51:47 +0100

    Igranje s Hello React

---
+++ 3_REACT/helloReact/src/App.jsx
@@ -0,0 +1,69 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import react from './assets/react.svg'
+import './App.css'
+import { Dropdown, Table } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={react} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <Button variant="danger">Danger</Button>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Container>
+  )
+}
+
+export default App
