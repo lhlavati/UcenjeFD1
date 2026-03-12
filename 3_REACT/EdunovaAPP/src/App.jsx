4f62705925072eca9d9e5fed83a125559a547aea
Luka Hlavati <lukahlavati@gmail.com>
2026-03-12 18:00:27 +0100

    Edunova predložak

--- 3_REACT/EdunovaAPP/src/App.jsx
+++ 3_REACT/EdunovaAPP/src/App.jsx
@@ -1,10 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Izbornik from './components/Izbornik'
 
 function App() {
   return (
-    <>
-      Hello Edunova
-    </>
+    <Container>
+      <Izbornik />
+    </Container>
   )
 }
 
