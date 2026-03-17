d2ff56da927317edf52cfe847dba7e3570c713c3
Luka Hlavati <lukahlavati@gmail.com>
2026-03-17 18:06:45 +0100

    osnovni prikaz podataka

--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react"
+import SmjerService from "../../services/smjerovi/SmjerService"
+
 export default function SmjerPregled(){
+
+
+    const [smjerovi, setSmjerovi] = useState([])
+
+    useEffect(() => {
+        ucitajSmjerove()
+    }, [])
+
+    async function ucitajSmjerove(){
+        await SmjerService.get().then((odgovor) => {
+            setSmjerovi(odgovor.data)
+        })
+    }
+
+
     return(
         <>
-        Ovdje će se vidjeti smjerovi
+            <ul>
+               {smjerovi && smjerovi.map((smjer) => (
+                <li>{smjer.naziv}</li>
+               ))} 
+            </ul>
         </>
     )
 }
