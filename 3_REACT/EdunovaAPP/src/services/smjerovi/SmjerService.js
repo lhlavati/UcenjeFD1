44ffa18f9470a5cc70cdd6b87828e138df52fadb
lhlavati <lukahlavati@gmail.com>
2026-03-26 17:15:11 +0100

    završetak promjene smjera

--- 3_REACT/EdunovaAPP/src/services/smjerovi/SmjerService.js
+++ 3_REACT/EdunovaAPP/src/services/smjerovi/SmjerService.js
@@ -2,7 +2,7 @@ import { smjerovi } from "./SmjerPodaci";
 
 // 1/4 Read od CRUD
 async function get() {
-    return {data: smjerovi}
+    return {data: [...smjerovi]}
 }
 
 async function getBySifra(sifra) {
